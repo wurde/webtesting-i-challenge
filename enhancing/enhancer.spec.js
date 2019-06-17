141b4e6a5870178ce4e3938941489ebe310dc754
Andy Bettisworth <andy@accreu.com>
2019-06-17 14:54:09 -0500

    Add TODO tests

--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,1 +1,29 @@
+'use strict'
+
+/**
+ * Dependencies
+ */
+
 const enhancer = require('./enhancer.js')
+
+/**
+ * Assertions
+ */
+
+describe("enhancer.js", () => {
+  describe("#succeed", () => {
+    test.todo("TODO")
+  })
+
+  describe("#fail", () => {
+    test.todo("TODO")
+  })
+
+  describe("#repair", () => {
+    test.todo("TODO")
+  })
+
+  describe("#get", () => {
+    test.todo("TODO")
+  })
+})
